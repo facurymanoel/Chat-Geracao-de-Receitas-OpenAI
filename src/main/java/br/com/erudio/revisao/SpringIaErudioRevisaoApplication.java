b3311b85eff9fcce7c63dd6bb88164b8bbcb75ca
Dalmo Facuri <mffacuri@terra.com.br>
2026-05-18 14:14:14 -0300

    Projeto criado

---
+++ src/main/java/br/com/erudio/revisao/SpringIaErudioRevisaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.erudio.revisao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringIaErudioRevisaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringIaErudioRevisaoApplication.class, args);
+	}
+
+}
