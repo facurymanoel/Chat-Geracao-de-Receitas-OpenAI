ac86249c1d13b9668f6c513fb8b418908264dd0d
Dalmo Facuri <mffacuri@terra.com.br>
2026-05-26 16:06:55 -0300

    Documentação interface AIService e classe MockAIService

--- src/main/java/br/com/erudio/revisao/service/AIService.java
+++ src/main/java/br/com/erudio/revisao/service/AIService.java
@@ -1,6 +1,19 @@
 package br.com.erudio.revisao.service;
 
+ /**
+  * Interface responsável pelo contrato de geração de receitas
+  * utilizando inteligência artificial
+  * 
+  * Permite diferentes implementações de IA sem alterar 
+  * a lógica de negócio da aplicação.
+  */
 public interface AIService {
-      
-	 String generateRecipe(String ingredients);
+	
+	/**
+	 * Gera uma receita com base nos ingredientes informados.
+	 *  
+	 * @param ingredients ingredientes fornecidos pelo usuário
+	 * @return receita gerada pela IA.
+	 */
+     String generateRecipe(String ingredients);
 }
--- src/main/java/br/com/erudio/revisao/service/MockAIService.java
+++ src/main/java/br/com/erudio/revisao/service/MockAIService.java
@@ -2,22 +2,43 @@ package br.com.erudio.revisao.service;
 
 import org.springframework.stereotype.Service;
 
+/**
+ * Implementação mock da interface AIService.
+ * 
+ * Utilizada para testes e desenvolvimento da aplicação
+ * sem depender de chamadas reais para serviços de
+ * Inteligência artificial.
+ * 
+ *  Essa abordagem permite validar o fluxo de aplicação
+ *  localmente mesmo quando a API externa estiver
+ *  indisponível ou com limite de utilização.
+ */
 @Service
 public class MockAIService implements AIService {
-
+    
+	/**
+	 * Gera uma receita simulada com base nos ingredientes
+	 * informados pelo usuário.
+	 * 
+	 * O retorno é fixo e utilizado apenas para fins
+	 * de testes e desenvolvimento local.
+	 * 
+	 * @param ingredients ingredientes fornecidos pelo usuário
+	 * @return receita simulada no formato de texto.
+	 */
 	@Override
 	public String generateRecipe(String ingredients) {
-		 
+
 		return """
-	               Receita Gerada (MOCK):
+				Receita Gerada (MOCK):
 
-	               Ingredientes: %s
+				Ingredientes: %s
 
-	               Modo de preparo:
-	               1. Misture tudo.
-	               2. Leve ao forno por 45 minutos.
-	               3. Sirva quente.
-	               """.formatted(ingredients);
+				Modo de preparo:
+				1. Misture tudo.
+				2. Leve ao forno por 45 minutos.
+				3. Sirva quente.
+				""".formatted(ingredients);
 	}
 
 }
